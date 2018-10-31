5b010b521115b352f3740fb82e0065f3e404a995
전형탁 <andy395023@gmail.com>
2018-10-31 12:05:36 +0900

    세팅

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var fs = require("fs");
+var passport= require('passport');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(session({
+    secret: '@#@$MYSIGN#@$#$',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(express.static('public'));
+
+mongoose.connect('mongodb://localhost/farmbot',{ useNewUrlParser: true })
+mongoose.Promise = global.Promise; 
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+var db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', function(){
+    console.log("Connnected to mongod server");
+});
+
+var router = require('./router/index')(app, Goout, passport);
+
+var server = app.listen(3000, function(){
+    console.log("Express server has started on port 3000");
+});
